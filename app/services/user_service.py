4cf3494aa9465651fc0e5ecb978fb866b8f01aea
Lev <lzelenin@njit.edu>
2025-05-01 14:49:24 -0400

    Fixing uuid in user service. Closes #28
    Signed-off-by: Lev <lzelenin@njit.edu>

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -18,6 +18,7 @@ import logging
 from fastapi import UploadFile
 from app.utils.minio_client import minio_client, bucket_name
 from app.utils.minio_client import ensure_bucket_exists
+import uuid
 
 settings = get_settings()
 logger = logging.getLogger(__name__)
